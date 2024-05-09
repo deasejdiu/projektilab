b33bbc0cdbee3e6a098167d63d12a40f56035c17
deasejdiu <ds64830@ubt-uni.net>
2024-05-09 20:54:16 +0200

    Cars+Crud

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,53 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
-import SignUp from "./pages/SignUpPage";
-import Login from "./pages/LoginPage";
-import Home from "./pages/HomePage";
-import BookCars from "./pages/BookCarsPage";
-import Rent from "./pages/RentPage";
-import Profile from "./pages/ProfilePage";
-import Dashboard from "./pages/DashboardPage";
-import NotFound from "./pages/Page404";
-import LoadingSpinner from "./components/ui/loading-spinner";
-import useAuthentication from "./useAuthentication";
-
 function App() {
-  const { isLoggedIn, isLoading } = useAuthentication();
-
-  if (isLoading) return <LoadingSpinner />;
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="login" element={<Login />} />
-        <Route path="cars" element={<BookCars />} />
-        <Route path="cars/:id" element={<Rent />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-
-// {isLoggedIn ? <Home /> : <Login />}
+    const [activeTab, setActiveTab] = useState('cars');
+    const [cars, setCars] = useState([]);
+  
+    useEffect(() => {
+      if (activeTab === 'cars') {
+        fetchAutos();
+      } else if (activeTab === 'packages') {
+        fetchPaketat();
+      } else {
+        fetchAllUser();
+      }
+    }, [activeTab]);
+  
+    const fetchAutos = () => {
+      fetch('http://localhost:8080/cars')
+        .then(response => response.json())
+        .then(data => setCars(data))
+        .catch(error => console.error('Error fetching cars:', error));
+    };
+  
+    // Other fetch functions...
+  
+    const handleTabChange = (event, newValue) => {
+      setActiveTab(newValue);
+    };
+  
+    return (
+      <div>
+        <AppBar position="static">
+          <Tabs value={activeTab} onChange={handleTabChange}>
+            <Tab label="Add Car" value="addCar" />
+            <Tab label="Delete Car" value="deleteCar" />
+            <Tab label="Update Car" value="updateCar" />
+            <Tab label="Autos" value="cars" />
+          </Tabs>
+        </AppBar>
+        <Container>
+          <Box mt={3}>
+            {activeTab === 'cars' && (
+              <div>
+                <Typography variant="h4">Autos</Typography>
+                <Autos cars={cars} />
+              </div>
+            )}
+          </Box>
+        </Container>
+      </div>
+    );
+  }
+  
+  export default App;
+  
